2bcd2f51ca5b2d3d6350248105e96c5051324deb
UnicornsNeigh <aball9423+1@gmail.com>
2022-09-01 19:19:16 -0400

    temp code

--- packages/library/rollup.config.js
+++ packages/library/rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import svg from "rollup-plugin-svg";
 
 const packageJson = require("./package.json");
 
@@ -23,6 +24,7 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
+      svg(),
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
   },
