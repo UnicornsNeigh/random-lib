27fbbd34bac3b72c265180a982c8c4f283dd3ae4
unicorn <alexanderball@Simon.local>
2022-09-12 18:43:21 -0400

    most up to date changes

--- packages/library/src/stories/Modal.stories.tsx
+++ packages/library/src/stories/Modal.stories.tsx
@@ -3,8 +3,6 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Modal } from "../components";
 
-// test change
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Example/Modal",
   component: Modal,
